205af6be0c9d5a6b8f8bbf14b80a4f03d41c38d6
César Pérez <cesar.alberto.perez@gmail.com>
2013-10-02 12:06:42 -0500

    - fix failing UTs

--- test/todo.spec.js
+++ test/todo.spec.js
@@ -1,31 +1,40 @@
 describe('TodoController', function () {
-    var controller, scope;
+    var controller, scope, rootScope, q, mockTodoService;
 
     beforeEach(module('todoApp'));
 
-    beforeEach(inject(function ($rootScope, $controller) {
+    beforeEach(inject(function ($rootScope, $controller, $q) {
         scope = $rootScope.$new();
+        q = $q;
+        rootScope = $rootScope;
+        mockTodoService = jasmine.createSpyObj('TodoService', ['addTodo']);
 
         controller = $controller('TodoController', {
-            $scope: scope
+            $scope: scope,
+            TodoService: mockTodoService
         });
     }));
 
-    it('should add the todo to the todoList when addTodo() called', function () {
+    it('should add the todo to the todoList when addTodo() called with success', function () {
         var expectedTodo = {description: 'test', project: 'other'};
+        var response = {data: expectedTodo};
         scope.todo = expectedTodo;
+        mockTodoService.addTodo.andReturn(q.when(response));
 
         scope.addTodo();
 
+        rootScope.$apply();
         expect(scope.todoList.length).toBe(1);
         expect(scope.todoList[0]).toBe(expectedTodo);
     });
 
     it('should assign a new object to the todo when addTodo() called', function () {
         scope.todo = {description: 'test', project: 'other'};
+        mockTodoService.addTodo.andReturn(q.when({data: scope.todo}));
 
         scope.addTodo();
 
+        rootScope.$apply();
         expect(scope.todo.description).toBeFalsy();
         expect(scope.todo.project).toBeFalsy();
     });
