ab86dd8e043801ddc57bc51311e3cf66b72f02aa
César Pérez <cesar.alberto.perez@gmail.com>
2013-10-04 11:05:16 -0500

    - pikaday directive example

--- client/app/directive.js
+++ client/app/directive.js
@@ -30,4 +30,46 @@ directiveDemo.controller('DemoController', function ($scope) {
             zipCode: '12345'
         }
     };
+
+    $scope.theDate = '01/02/2013';
+});
+
+directiveDemo.directive('datePicker', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+            var dateFormat = attrs.dateFormat,
+                picker;
+
+            picker = new Pikaday({
+                field: element[0],
+                format: dateFormat,
+                onSelect: function () {
+                    var date = this.getMoment().format(dateFormat);
+
+                    // if angular is already updating the values we just set the new value
+                    if (scope.$$phase || scope.$root.$$phase) {
+                        ngModel.$setViewValue(date);
+                    } else {
+                        // we have to call scope.$apply to let angular know that the value changed
+                        scope.$apply(function () {
+                            ngModel.$setViewValue(date);
+                        });
+                    }
+                }
+            });
+
+            ngModel.$render = function () {
+                if (ngModel.$viewValue) {
+                    picker.setMoment(moment(ngModel.$viewValue, dateFormat));
+                }
+            };
+
+            // we have to do some cleanup when the element is going to be destroyed
+            element.bind('$destroy', function () {
+                picker.destroy();
+            });
+        }
+    };
 });
