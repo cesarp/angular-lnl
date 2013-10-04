deaabce33b8cc8b0f5d8cb67c641328d2bf473b1
César Pérez <cesar.alberto.perez@gmail.com>
2013-10-04 08:58:56 -0500

    - simple directive example

--- client/app/directive.js
+++ client/app/directive.js
@@ -1,1 +1,33 @@
 var directiveDemo = angular.module('directiveDemo', []);
+
+directiveDemo.directive('dealerInfo', function () {
+    return {
+        restrict: 'EA',
+        scope: {
+            dealer: '='
+        },
+        link: function (scope, element, attrs) {
+            element.on('click', function () {
+                element.toggleClass('highlight');
+            });
+        },
+        template: '<strong>{{ dealer.name }}</strong>' +
+            '<address>' +
+            '{{ dealer.address.addressLine1 }} <br/>' +
+            '{{ dealer.address.city }}, <br/>' +
+            '{{ dealer.address.stateCode }} {{ dealer.address.zipCode }}' +
+            '</address>'
+    };
+});
+
+directiveDemo.controller('DemoController', function ($scope) {
+    $scope.theDealer = {
+        name: 'A DEALER NAME',
+        address: {
+            addressLine1: '123 STREET NAME',
+            city: 'CITY',
+            stateCode: 'STATE',
+            zipCode: '12345'
+        }
+    };
+});
