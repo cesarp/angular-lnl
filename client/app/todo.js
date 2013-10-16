c584c7fc908f73eba5d6dc553c7ca44883e9a618
César Pérez <cesar.alberto.perez@gmail.com>
2013-10-16 10:41:38 -0500

    - added $resource comments

--- client/app/todo.js
+++ client/app/todo.js
@@ -31,4 +31,22 @@ todoApp.controller('TodoController', function ($scope, Todo) {
 // define a resource instead of using the $http module
 todoApp.factory('Todo', function ($resource) {
     return $resource('/todo/:id');
+
+    /*
+     'get':    {method:'GET'},      /path/params?moreParams expects one object back
+     'save':   {method:'POST'},     /path/ or /path/id for save/update
+     'query':  {method:'GET'},      /path    to get a list back
+     'remove': {method:'DELETE'},   /path/id to remove it
+     'delete': {method:'DELETE'}    /path/id the same as remove
+
+     MyResource.query({done:false}) becomes GET /path?done=false
+
+     You can also create new instances
+     var instance = new MyResource();
+
+     now you get some instance methods too
+     instance.$save();
+     instance.$remove();
+     instance.$delete();
+     */
 });
